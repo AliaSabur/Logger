8d4bd5c8064e1520293ae71548f609b1f8bf4d20
AliaSabur <155058857+AliaSabur@users.noreply.github.com>
2024-06-16 21:01:22 +0800

    Update logger.h

--- logger.h
+++ logger.h
@@ -23,7 +23,7 @@ private:
 
 	static constexpr size_t BUFFER_SIZE = 1024;
 	Level level;
-	TCHAR fileName[MAX_PATH];
+	char fileName[MAX_PATH];
 	std::atomic<size_t> head{ 0 }, tail{ 0 };
 	std::vector<LogEntry> buffer;
 	HANDLE logThread;
@@ -35,26 +35,27 @@ private:
 		return 0;
 	}
 
-	const TCHAR* levelToString(Level level) const {
+	const char* levelToString(Level level) const {
 		switch (level) {
-		case LOG_DEBUG: return _T("DEBUG");
-		case LOG_INFO:  return _T("INFO");
-		case LOG_WARN:  return _T("WARN");
-		case LOG_ERROR: return _T("ERROR");
-		default:        return _T("UNKNOWN");
+		case LOG_DEBUG: return "DEBUG";
+		case LOG_INFO:  return "INFO";
+		case LOG_WARN:  return "WARN";
+		case LOG_ERROR: return "ERROR";
+		default:        return "UNKNOWN";
 		}
 	}
 
-	TCHAR* currentTime() const {
-		static TCHAR buffer[20];
+	char* currentTime() const {
+		static char buffer[25] = { 0 };
 		SYSTEMTIME st;
 		GetLocalTime(&st);
-		_stprintf_s(buffer, _countof(buffer), _T("%04d-%02d-%02d %02d:%02d:%02d"), st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+		// _stprintf_s(buffer, _countof(buffer), _T("%04d-%02d-%02d %02d:%02d:%02d"), st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+		sprintf_s(buffer, _countof(buffer), "%04d-%02d-%02d %02d:%02d:%02d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
 		return buffer;
 	}
 
 	void processLogQueue() {
-		HANDLE hFile = CreateFile(fileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		HANDLE hFile = CreateFileA(fileName, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 		SetFilePointer(hFile, 0, NULL, FILE_END);
 
 		while (running) {
@@ -75,8 +76,7 @@ private:
 	}
 
 	// Helper function for conversion
-	std::string ConvertToUTF8(const TCHAR* message) {
-#ifdef UNICODE
+	std::string ConvertToUTF8(const wchar_t* message) {
 		int bufferLength = WideCharToMultiByte(CP_UTF8, 0, message, -1, nullptr, 0, nullptr, nullptr);
 		if (bufferLength == 0) {
 			// throw std::runtime_error("Failed to get buffer length for UTF-8 conversion.");
@@ -87,14 +87,12 @@ private:
 		WideCharToMultiByte(CP_UTF8, 0, message, -1, &utf8Message[0], bufferLength, nullptr, nullptr);
 		utf8Message.resize(bufferLength - 1);  // Remove the null terminator added by WideCharToMultiByte
 		return utf8Message;
-#else
-		return std::string(message);
-#endif
 	}
 
 public:
-	Logger(Level lvl, const TCHAR* fName) : level(lvl), buffer(BUFFER_SIZE) {
-		_tcsncpy_s(fileName, fName, MAX_PATH);
+	Logger(Level lvl, const char* fName) : level(lvl), buffer(BUFFER_SIZE) {
+		// _tcsncpy_s(fileName, fName, MAX_PATH);
+		strncpy_s(fileName, _countof(fileName), fName, _TRUNCATE);
 		logThread = CreateThread(NULL, 0, ThreadFunc, this, 0, NULL);
 	}
 
@@ -104,48 +102,79 @@ public:
 		CloseHandle(logThread);
 	}
 
-	void log(Level msgLevel, const TCHAR* message) {
-		// std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-		// std::string utf8Message = converter.to_bytes(message);  // Convert from TCHAR (UTF-16) to UTF-8
-		std::string utf8Message = ConvertToUTF8(message);
+	void log(Level msgLevel, const char* message) {
+		
+		std::string utf8Message(message);
 
 		auto nextHead = (head.load(std::memory_order_relaxed) + 1) % BUFFER_SIZE;
 		while (nextHead == tail.load(std::memory_order_acquire));
 
-		// std::string formattedMessage = converter.to_bytes(currentTime()) + " [" + converter.to_bytes(levelToString(msgLevel)) + "] " + utf8Message + "\n";
-		std::string formattedMessage = ConvertToUTF8(currentTime()) + " [" + ConvertToUTF8(levelToString(msgLevel)) + "] " + utf8Message + "\n";
+		std::string formattedMessage = std::string(currentTime()) + " [" + std::string(levelToString(msgLevel)) + "] " + utf8Message + "\n";
 		buffer[head].message = formattedMessage;
 		buffer[head].ready.store(true, std::memory_order_release);
 		head.store(nextHead, std::memory_order_release);
 	}
 
 	template<typename... Args>
-	void logf(Level msgLevel, const TCHAR* format, Args... args) {
+	void logf(Level msgLevel, const wchar_t* format, Args... args) {
 		// Assume formatting in TCHAR, convert to UTF-8 post formatting
-		int requiredSize = _sctprintf(format, args...) + 1; // Calculate required size for the buffer
-		std::unique_ptr<TCHAR[]> formattedMessage(new TCHAR[requiredSize]);
-		_stprintf_s(formattedMessage.get(), requiredSize, format, args...);
+		// int requiredSize = _sctprintf(format, args...) + 1; // Calculate required size for the buffer
+		int requiredSize = _scwprintf(format, args...) + 1;
+		std::unique_ptr<wchar_t[]> formattedMessage(new wchar_t[requiredSize]);
+		// _stprintf_s(formattedMessage.get(), requiredSize, format, args...);
+		swprintf_s(formattedMessage.get(), requiredSize, format, args...);
+		// log(msgLevel, formattedMessage.get());
+		log(msgLevel, ConvertToUTF8(formattedMessage.get()).c_str());
+	}
+
+	template<typename... Args>
+	void logf_utf8(Level msgLevel, const char* format, Args... args) {
+		// Assume formatting in TCHAR, convert to UTF-8 post formatting
+		int requiredSize = _scprintf(format, args...) + 1; // Calculate required size for the buffer
+		std::unique_ptr<char[]> formattedMessage(new char[requiredSize]);
+		// _stprintf_s(formattedMessage.get(), requiredSize, format, args...);
+		sprintf_s(formattedMessage.get(), requiredSize, format, args...);
 		log(msgLevel, formattedMessage.get());
 	}
 
 	template<typename... Args>
-	void debug(const TCHAR* format, Args... args) {
+	void debug(const wchar_t* format, Args... args) {
 		logf(LOG_DEBUG, format, args...);
 	}
 
 	template<typename... Args>
-	void info(const TCHAR* format, Args... args) {
+	void debug_utf8(const char* format, Args... args) {
+		logf_utf8(LOG_DEBUG, format, args...);
+	}
+
+	template<typename... Args>
+	void info(const wchar_t* format, Args... args) {
 		logf(LOG_INFO, format, args...);
 	}
 
 	template<typename... Args>
-	void warn(const TCHAR* format, Args... args) {
+	void info_utf8(const char* format, Args... args) {
+		logf_utf8(LOG_INFO, format, args...);
+	}
+
+	template<typename... Args>
+	void warn(const wchar_t* format, Args... args) {
 		logf(LOG_WARN, format, args...);
 	}
 
 	template<typename... Args>
-	void error(const TCHAR* format, Args... args) {
+	void warn_utf8(const char* format, Args... args) {
+		logf_utf8(LOG_WARN, format, args...);
+	}
+
+	template<typename... Args>
+	void error(const wchar_t* format, Args... args) {
 		logf(LOG_ERROR, format, args...);
+	}
+
+	template<typename... Args>
+	void error_utf8(const char* format, Args... args) {
+		logf_utf8(LOG_ERROR, format, args...);
 	}
 };
 
